936c50a1c5c08c24de670349fd45d83cbf7fc557
Mofasel006 <mofasel.iit@gmail.com>
2026-08-08 00:36:05 +0600

    Clean up express route types

--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,23 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
-var __filename = fileURLToPath(import.meta.url);
-var __dirname = path.dirname(__filename);
-var app = express();
-var staticPath = path.resolve(__dirname, "..", "dist", "public");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const staticPath = path.resolve(__dirname, "..", "dist", "public");
 
 app.use(express.static(staticPath));
 
-app.get("*", (_req: Request, res: Response<any, Record<string, any>>) => {
-  (res as any).sendFile(path.join(staticPath, "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(staticPath, "index.html"));
 });
 
-var index_default = app;
+export default app;
 
 if (process.env.NODE_ENV !== "production") {
-  const port = process.env.PORT || 3e3;
+  const port = process.env.PORT || 3000;
   app.listen(port, () => {
     console.log(`Server running locally on http://localhost:${port}/`);
   });
 }
-
-export {
-  index_default as default
-};
